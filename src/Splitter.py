9b69e20e46f3c1a1c1bf78ccd8a91d682fdc31ff
Mark ten Klooster <tenklooster.mark@gmail.com>
2023-05-08 19:19:00 +0200

    added parameter to splitter to specify start index

--- src/Splitter.py
+++ src/Splitter.py
@@ -6,11 +6,13 @@ from pydub.silence import split_on_silence
 class Splitter:
 
     def __init__(self,
+                 start_index: int = 0,
                  min_silence_len: int = 1000,
                  silence_thresh: int = -16,
                  keep_silence: int = 100,
                  seek_step: int = 1,
                  ):
+        self.start_index = start_index
         self.min_silence_len = min_silence_len
         self.silence_thresh = silence_thresh
         self.keep_silence = keep_silence
@@ -21,7 +23,7 @@ class Splitter:
         audio_chunks = split_on_silence(sound_file, self.min_silence_len, self.silence_thresh, self.keep_silence,
                                         self.seek_step)
         for i, chunk in enumerate(audio_chunks):
-            out_file = os.path.join(output_dir, f"word_{i}.wav")
+            out_file = os.path.join(output_dir, f"word_{i+self.start_index}.wav")
             print("exporting", out_file)
             chunk.export(out_file, format="wav")
 
