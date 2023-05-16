779b1560b384a1cfce5f482a25774ea59d27d1ab
Mark ten Klooster <tenklooster.mark@gmail.com>
2023-05-16 19:04:32 +0200

    imported last file

--- src/pydub-splitter.py
+++ src/pydub-splitter.py
@@ -1,12 +1,12 @@
 import os
 from src.Splitter import Splitter
 
-RECORDING_FILE = os.path.join('../recordings', 'page_7_16k.wav')
+RECORDING_FILE = os.path.join('../recordings', 'page_13_16k.wav')
 SAMPLE_RATE = 16000
 
 
 if __name__ == '__main__':
-    splitter = Splitter(600, 300, -55, 100, 1)
+    splitter = Splitter(1200, 300, -55, 100, 1)
     splitter.split(RECORDING_FILE, './../recordings/segments/')
 
 
